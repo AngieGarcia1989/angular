9e4824a4453d2e93d6cd08dcad803c2544a7b516
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-10-17 21:40:09 -0500

    uso de deleteItem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platzi-store'; 
   items =['angie', 'andres', 'juan'];
-  
+
   addItem() {
   this.items.push('nuevo item');
+  }
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
   }
 }
